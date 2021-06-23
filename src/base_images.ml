a6b1af4ac563a36c21eb8c2d26d0ad3b4c815d79
Antonin Décimo <antonin@tarides.com>
2021-06-23 13:13:38 +0200

    Support Logs.cli

--- src/base_images.ml
+++ src/base_images.ml
@@ -5,7 +5,9 @@ let program_name = "base_images"
 
 module Rpc = Current_rpc.Impl(Current)
 
-let () = Prometheus_unix.Logging.init ()
+let setup_log default_level =
+  Prometheus_unix.Logging.init ?default_level ();
+  ()
 
 (* A low-security Docker Hub user used to push images to the staging area.
    Low-security because we never rely on the tags in this repository, just the hashes. *)
@@ -56,7 +58,7 @@ let has_role user = function
            ) -> true
     | _ -> false
 
-let main config mode channel capnp_address github_auth submission_uri staging_password_file =
+let main () config mode channel capnp_address github_auth submission_uri staging_password_file =
   Lwt_main.run begin
     let channel = Option.map read_channel_uri channel in
     let staging_auth = staging_password_file |> Option.map (fun path -> staging_user, read_first_line path) in
@@ -129,10 +131,14 @@ let staging_password =
     ~docv:"FILE"
     ["staging-password-file"]
 
+let setup_log =
+  Term.(const setup_log $ Logs_cli.level ())
+
 let cmd =
   let doc = "Build the ocaml/opam images for Docker Hub" in
-  Term.(term_result (const main $ Current.Config.cmdliner $ Current_web.cmdliner $ slack $ capnp_address $ Current_github.Auth.cmdliner $
-                     submission_service $ staging_password)),
+  Term.(term_result (const main $ setup_log $ Current.Config.cmdliner $ Current_web.cmdliner
+                     $ slack $ capnp_address $ Current_github.Auth.cmdliner $ submission_service
+                     $ staging_password)),
   Term.info program_name ~doc
 
 let () = Term.(exit @@ eval cmd)
